a0d4e33a3c5aa8a812ed4a3517014a27ba2ba935
dcol2804 <50344360+dcol2804@users.noreply.github.com>
2022-03-03 13:49:21 +1100

    Update API examples v1.R
    Cleaned comments further, added an example that subsets species to multiple taxon values, added example of how to return a csv file as a download

--- API.build/API examples v1.R
+++ API.build/API examples v1.R
@@ -8,12 +8,8 @@
 #install.packages("plumber")
 library(plumber)
 
-
-#install.packages("tidyverse)
+#install.packages("tidyverse")
 library(tidyverse)
-
-#install.packages("remotes")
-#remotes::install_github("traitecoevo/austraits", build_vignettes = TRUE)
 
 
 #install.packages("remotes")
@@ -39,25 +35,9 @@ status = function(){
 ################################################################################
 # Log some information about the incoming request
 
-# The filter function allows some action to be done before the incoming query is passed on (plumber::forward()) to the relevant @get function
-
-#* Log some information about the incoming request
-#* @filter logger
-function(req){
-  cat(as.character(Sys.time()), "-",
-      req$REQUEST_METHOD, req$PATH_INFO, "-",
-      req$HTTP_USER_AGENT, "@", req$REMOTE_ADDR, "\n")
-  plumber::forward()
-}
-
-
-
-
-
-
 # The @filter function in r plumber allows some action to be completed 
 # before the incoming request is passed on (using plumber::forward()) to the relevant @get function. 
-# This example logs some information about the incoming request. 
+# This example logs some information about the incoming request, the time, the path and the remote address 
 # The @filter function is also a way to implement an authentication step before data can be accessed.
 
 #* Log some information about the incoming request
@@ -76,8 +56,8 @@ function(req){
 #* @apiDescription 
 #* Return a count of unique traits for any given taxa from the taxon_name data field in AusTraits., e.g. Eucalyptus saligna 
 
-#* @param taxa
-#* @get /trait_count
+#* @param taxa e.g. Angophora costata
+#* @get /trait-count
 
 function(taxa = ""){
   
@@ -104,19 +84,19 @@ function(taxa = ""){
   
   }else{
     
-    print("No data can be found for this taxon name")
+    print("No data can be found for this taxon name. Check the spelling and try again")
   }
  }
 }
 
 
-#######################################################################
+################################################################################
 # 2. count the number of taxa per trait
 
 #* @apiDescription Possible values for traits are found at http://traitecoevo.github.io/austraits.build/articles/austraits_database_structure.html
 #* Return a count of unique species for any given trait name in the trait_name data field of AusTraits.
-#* @param trait
-#* @get /taxa_count
+#* @param trait e.g. leaf_length
+#* @get /taxa-count
 
 function(trait = ""){
   
@@ -153,14 +133,14 @@ function(trait){
 }
 
 
-########################################################
+################################################################################
 # 3. Present mean values for each trait associated with a taxa
 
 #* @apiDescription 
 #* Return a table of summarised trait data for a given taxa from the taxon_name data field in AusTraits
 
-#* @param taxa
-#* @get /trait_summary
+#* @param taxa e.g. Angophora costata
+#* @get /trait-summary
 
 function(taxa = ""){
   
@@ -170,19 +150,25 @@ function(taxa = ""){
     
   }else{
   
+    # subset the data to the desired taxon name and select the trait name, value and unit columns
   summary = austraits$traits %>% 
     filter(taxon_name == taxa) %>% 
     select(trait_name, value, unit) 
   
+  # split the data into categorical variables 
   categorical = summary %>% filter(is.na(unit))  %>% filter(duplicated(trait_name)) %>% distinct() 
   
+  # and continuous variables
   continuous = summary %>% filter(!is.na(unit)) %>% mutate(value = as.numeric(value)) %>% group_by(trait_name) %>%  summarise(value = mean(value), unit = unique(unit)) 
   
+  # reduce to 3 significant figures
   continuous$value = signif(continuous$value, 3)
   
+  #stack them
   x = rbind(categorical, continuous)
   
-  if (length(x$value) != 0){
+  # only print the data if data exists
+  if (nrow(x) != 0){
   
   print(x)
     
@@ -193,21 +179,51 @@ function(taxa = ""){
    }
   }
 }
-######################################################################
+################################################################################
 # 4. Presents a table of the combined traits and methods tables for a given taxa in csv format. 
 # Possible filters are listed.
-# Add the %in% function 
-# 
 
 
 #* @apiDescription 
-#* Return a full data table for given taxa from the taxon_name data field in AusTraits. Leaving the taxa field blank returns the full dataset.
-#* @param 
-#* @get /trait_table
+#* Return a full data table for given taxa from the taxon_name data field in AusTraits.
+#* @param taxa:character e.g. Angophora costata
+#* @param collection:character e.g. field, glasshouse
+#* @param type_of_value:character e.g. mean, min,
+#* @get /trait-table
 
-function(taxa = "", collection = "", type_of_value = ""){
+function(res, taxa = "", collection = "", type_of_value = ""){
   
   
+  # filter the data to the desired taxon name
+  x1 = austraits$traits %>% filter(taxon_name %in% taxa) 
+  
+  # get the methods table of austraits
+  x2 = austraits$methods
+  
+  # merge the traits table with the methods table by the field names they have in common
+  x3 = merge(x1, x2, by = intersect(names(x1), names(x2))) 
+  
+  # select the fields for the wide table format
+  x3 %>% select(dataset_id, taxon_name, value, unit, value_type, date, collection_type) %>%
+         filter(str_detect(collection_type, collection)) %>% 
+         filter(str_detect(value_type, type_of_value))
+
+}
+
+################################################################################
+# 5. Post a list of species names and return a full data table of multiple species as a csv file
+
+#* @apiDescription 
+#* Return a full data table for given taxa for multiple species names as a csv file
+#* @serializer csv
+#* @param taxa_list:[object] list of taxa such as "Angophora costata", "Eucalyptus regnans"
+#* @post /trait-table-download-csv
+
+function(req, res){
+  
+  # make taxa object = the taxa_list inside the request body
+  taxa = req$body$taxa_list
+
   #### 3. Make wide tables
   
   x1 = austraits$traits %>% filter(taxon_name %in% taxa) 
@@ -219,16 +235,18 @@ function(taxa = "", collection = "", type_of_value = ""){
   x3 = merge(x1, x2, by = intersect(names(x1), names(x2))) 
   
   # select the fields for the wide table format
-  x3 %>% select(dataset_id, taxon_name, value, unit, value_type, date, collection_type) %>%
-         filter(str_detect(collection_type, collection)) %>% 
-         filter(str_detect(value_type, type_of_value))
+  x4 = x3 %>% select(dataset_id, taxon_name, value, unit, value_type, date, collection_type)
   
+  #prepare the filename
+  filename = str_c("AusTraits_", gsub("\\:", "-", Sys.time()), ".csv")
+  filename = gsub(" ", "_", filename)
   
-
+  #convert the subset to a csv
+  as_attachment(x4, filename)
+  
 }
 
-# #* @plumber 
-# function(pr){
-#   pr %>% 
-#     pr_set_api_spec(yaml::read_yaml("AusTraits_API_test_1.yml"))
-# }
+
+################################################################################
+
+
