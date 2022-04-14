a730d5b4c07fa1ab32f8cc1d7d45ebc88daaa9b9
dcol2804 <50344360+dcol2804@users.noreply.github.com>
2022-04-14 14:35:26 +1000

    Added limit to species numbers
    I added a limit of 2000 species to to the /trait-table-download-csv endpoint. Also updated the /trait-summary endpoint to return two tables - one for categorical traits and one for numeric traits

--- API.build/API examples v1.R
+++ API.build/API examples v1.R
@@ -14,130 +14,9 @@ library(stringr)
 library(readr)
 library(purrr)
 #install.packages("remotes")
-remotes::install_github("traitecoevo/austraits@api")
+remotes::install_github("traitecoevo/austraits")
 library(austraits) 
-austraits <- load_austraits(path="data/austraits", version="3.0.2")
-
-
-################################################################################
-as_wide_table <- function(austraits){
-  
-  
-  ################################################################################
-  # TODO: this updated with next zenodo release
-  # Load the trait classification doc - classifies the tissue type and type of trait based on the trait_name data field
-  # Exclude this for now -- will be added to definitions file in future release
-  # trait_class = read.csv("data-raw/Trait_classifications_v3.csv")
-  # trait_class[is.na(trait_class)] = ""
-  # trait_class <- trait_class %>% as_tibble()
-  # 
-  # we only need two extra columns from the trait class table - collapsing two category and other_tags cols and renaming them for clarity
-  # x2 <- 
-  #   trait_class %>% dplyr::mutate(
-  #   trait_category = str_c(category, "; ", other_tags) %>% gsub("; $", "", .)
-  # ) %>% 
-  #   dplyr::select(trait_name, tissue, trait_category)
-  # 
-  # Function to collapse columns in sites and contexts into single column
-  process_table <- function(data) {
-    
-    # worker function called intop worklfow below
-    # for a df, combine all column names and values
-    collapse_cols <- function(data) {
-      
-      if(ncol(data) ==0) return(NA_character_)
-      
-      data %>% purrr::imap_dfr(~ sprintf("%s='%s'",.y,.x)) %>%
-        tidyr::unite("text", sep="; ") %>% dplyr::pull(text)
-    }
-    
-    data %>% 
-      tidyr::pivot_wider(names_from = property, values_from = value) %>% 
-      tidyr::nest(data=-any_of(c("dataset_id", "site_name", "context_name", "latitude (deg)", "longitude (deg)"))) %>%
-      dplyr::mutate(site = purrr::map_chr(data, collapse_cols)) %>%
-      dplyr::select(-data) 
-  }
-  
-  ################################################################################
-  # Define and adapt each table in the list of austraits to prepare for the wide table format 
-  
-  # the trait table needs little prep. Rename the value columns as value
-  austraits$traits <- 
-    austraits$traits %>% 
-    dplyr::rename(c("trait_value" = "value")) 
-  
-  # The contexts table needs the contexts collapsed to one context name per site
-  austraits$contexts <- 
-    austraits$contexts %>% 
-    dplyr::rename(c("property" = "context_property")) %>%
-    split(austraits$contexts$dataset_id) %>%
-    purrr::map_dfr(process_table)  %>% 
-    dplyr::rename(c("context" = "site"))
-  
-  # Getting rid of the columns that will soon be deleted in the next austraits release and renaming the description column
-  austraits$methods <- 
-    austraits$methods %>% 
-    #  -----------
-  # TODO: this section can be removed for next release
-  # Some studies have multiple records per traits. This breaks things when joining
-  # For now select first
-  dplyr::group_by(dataset_id, trait_name) %>% 
-    dplyr::slice(1) %>%
-    dplyr:: ungroup() %>%
-    #------------
-  dplyr::select(-year_collected_start, -year_collected_end) %>% 
-    dplyr::rename(c("dataset_description" = "description"))  
-  
-  # collapse into one column
-  austraits$sites <- 
-    austraits$sites %>% 
-    dplyr::filter(value!="unkown") %>% 
-    # next line is a fix -- one dataset in 3.0.2 has value "site_name"
-    dplyr::mutate(site_property = gsub("site_name", "name", site_property)) %>%
-    dplyr::rename(c("property" = "site_property")) %>%
-    split(., .$dataset_id) %>%
-    purrr::map_dfr(process_table)
-  
-  # rename source data field to reflect the APC/APNI name matching process better
-  austraits$taxa <- 
-    austraits$taxa %>% 
-    dplyr::rename(c("taxonNameValidation" = "source"))
-  
-  austraits_wide <- 
-    austraits$traits %>%
-    dplyr::left_join(by=c("dataset_id", "context_name"), austraits$contexts) %>%
-    dplyr::left_join(by=c("dataset_id", "site_name"), austraits$sites) %>%
-    dplyr::left_join(by=c("dataset_id", "trait_name"), austraits$methods) %>%
-    dplyr::left_join(by=c("taxon_name"), austraits$taxa) %>%
-    
-    # reorder the names to be more intuitive
-    dplyr::select(
-      
-      # The most useful (if you are filtering for just one taxon_name)
-      dataset_id, observation_id, trait_name, taxon_name, trait_value, unit, 
-      value_type, replicates, 
-      # tissue, trait_category,  # Add after new zenodo release
-      
-      # More stuff you can filter on
-      date, collection_type, sample_age_class, sampling_strategy, 
-      
-      #stuff relating to sites
-      `latitude (deg)`, `longitude (deg)`, site_name, site,
-      
-      #stuff relating to contexts and methods
-      context_name, context, methods, original_name,
-      
-      #the citations
-      dataset_description, source_primary_citation, source_secondary_citation,
-      
-      #the taxa details
-      taxonomicStatus, taxonDistribution, 
-      taxonRank, genus, family, acceptedNameUsageID, 
-      scientificNameAuthorship, ccAttributionIRI
-    )
-  
-  austraits_wide
-}
+austraits <- load_austraits(path="data/austraits", version = get_version_latest())
 
 austraits_wide = as_wide_table(austraits)
 
@@ -273,28 +152,76 @@ function(taxa = ""){
     print("No taxon name has been entered")
     
   }else{
+  ############################ manipulate austraits to prepare for averages #################
+    ### to do later ###
+    
+    data = austraits$traits
+    #only select adult plants grown outdoors and not experimental data. Or perhaps a list of set traits that can be applied to the whole species
+    
+    data$value_type[which(is.na(data$value_type))] = "unknown"
+    
   
-    # subset the data to the desired taxon name and select the trait name, value and unit columns
-  summary = austraits$traits %>% 
-    filter(taxon_name == taxa) %>% 
-    select(trait_name, value, unit) 
+  ###################### Make the categorical trait summary  ####################
+    
+  # subset the data to the desired taxon name and get a vector of the available categorical traits
+  cat_traits = data %>% filter(taxon_name == taxa) %>% filter(is.na(unit)) %>% select(trait_name) %>% unique()
   
-  # split the data into categorical variables 
-  categorical = summary %>% filter(is.na(unit))  %>% filter(duplicated(trait_name)) %>% distinct() 
+  #create a blank dataframe
+  output = data.frame()
   
-  # and continuous variables
-  continuous = summary %>% filter(!is.na(unit)) %>% mutate(value = as.numeric(value)) %>% group_by(trait_name) %>%  summarise(value = mean(value), unit = unique(unit)) 
+  # for each of the categorical traits...
+  for (i in 1:length(cat_traits$trait_name)){
   
-  # reduce to 3 significant figures
-  continuous$value = signif(continuous$value, 3)
+  # get austraits data for this taxa and the trait, remove NA values and make a table of the counts for each value
+  x = data %>% filter(taxon_name == taxa & trait_name == cat_traits$trait_name[i] & is.na(value)==F) %>% select(value) %>% table() %>% as.data.frame()
   
-  #stack them
-  x = rbind(categorical, continuous)
+  # make a character string made up of each trait value, followed by the count in brackets and separated by ";"
+  y = paste0(str_c(x$., " (", x$Freq, ")"), collapse = "; ")
+  
+  # make a row of data made up of the taxon name, the trait name and the trait value character above (y). The units will be blank.
+  z = data.frame(taxon_name = taxa, trait_name = cat_traits$trait_name[i], trait_values = y, units = "")
+  
+  #glue it to the dataframe
+  output = rbind(output, z)
+  }
+  
+  ###################### Make the numeric trait summary  ####################
+  
+  num_traits = austraits$traits %>% filter(taxon_name == taxa) %>% filter(!is.na(unit)) %>% select(trait_name, unit) %>% unique()
+  
+  #create a blank dataframe
+  output1 = data.frame()
+  
+  # for each of the categorical traits...
+  for (i in 1:length(num_traits$trait_name)){
+    
+  # get the data for each numeric trait, group by the value type and find the mean
+  x = austraits$traits %>% filter(taxon_name == taxa & trait_name == num_traits$trait_name[i] & is.na(value)==F) %>% 
+      mutate(value = as.numeric(value)) %>% 
+      group_by(value_type) %>% summarise(mean = mean(value), n = n())
+    
+  # collapse each column to create just one summary column
+  y = x %>% mutate(summary = str_c(x$value_type,": ", signif(x$mean, 2), " (", signif(x$n, 2), ")")) #%>% select(value_type, summary) %>%
+    
+  # divide each summary value into either min, mean or max and collapse common values together.
+  y1 = data.frame(min = paste0(y$summary[grepl("min", y$summary)], collapse = ", "), 
+                  mean = paste0(y$summary[grepl("mean|individual|raw|literature|unknown", y$summary)], collapse = ", "),
+                  max = paste0(y$summary[grepl("max", y$summary)], collapse = ", "))
+    
+  # turn it into just one row of data with the species, the trait and the values
+  z = data.frame(taxon_name = taxa, trait_name = num_traits$trait_name[i], y1, unit = num_traits$unit[i])
+    
+    output1 = rbind(output1, z)
+    
+  }
+  
+  #Present as two names tables, categorical and numeric traits
+  summary = list(categorical_traits = output, numeric_traits = output1)
   
   # only print the data if data exists
-  if (nrow(x) != 0){
+  if (nrow(output)+nrow(output1) != 0){
   
-  print(x)
+  print(summary)
     
 }else{
   
@@ -348,32 +275,30 @@ function(req, res){
   
   # make taxa object = the taxa_list inside the request body
   taxa = req$body$taxa_list
-
+  
+  if (length(taxa) > 2000){
+    
+     taxa = taxa[1:2000]
+    
+     
+    }else{
   
   #subset
   x1 = austraits_wide %>% filter(taxon_name %in% taxa) 
+    }
   #x1[is.na(x1)] = ""
   #prepare the filename
   filename = str_c("AusTraits_", gsub("\\:", "-", Sys.time()), ".csv")
   filename = gsub(" ", "_", filename)
-  
   #convert the subset to a csv
   as_attachment(x1, filename)
+ 
   
 }
-
 
 
 ################################################################################
-# 6. Return the entire Austraits database
-#* @apiDescription 
-#* Return the entire Austraits database as a json object
-#* @get /all-austraits
 
-austraits_all = function(){
-return(austraits_wide)
- 
-}
   
 
 
